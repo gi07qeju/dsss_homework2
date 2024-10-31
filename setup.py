d062064e7f024bff720d1980bfd50b1c831db910
Lena Wiedholz <lena.wiedholz@fau.de>
2024-10-31 11:49:31 +0100

    files to make project pip installable

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dsss_homework2',
+    version='0.1.0',
+    description='A short description of your project',
+    author='Lena Wiedholz',
+    author_email='lena.wiedholz@fau.de',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  # Update as needed
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
